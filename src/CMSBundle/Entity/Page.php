3d1f9dfb1702baa5a30c926c3b5ea8fa1ea43e78
Léonard <picot.leonard@gmail.com>
2017-07-12 01:47:35 +0200

    Mise en place de l'entite Page

---
+++ src/CMSBundle/Entity/Page.php
@@ -0,0 +1,251 @@
+<?php
+
+namespace CMSBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use AppBundle\Entity\User;
+/**
+ * Page
+ *
+ * @ORM\Table(name="page")
+ * @ORM\Entity(repositoryClass="CMSBundle\Repository\PageRepository")
+ */
+class Page
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\User")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $user;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=255)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="contenu", type="text", nullable=true)
+     */
+    private $contenu;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="lien", type="string", length=255)
+     */
+    private $lien;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="topPublic", type="boolean")
+     */
+    private $topPublic;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="topPrivate", type="boolean")
+     */
+    private $topPrivate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255, nullable=true)
+     */
+    private $password;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set user
+     *
+     * @param User $user
+     *
+     * @return Page
+     */
+    public function setUser(User $user)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+
+    /**
+     * Set titre
+     *
+     * @param string $titre
+     *
+     * @return Page
+     */
+    public function setTitre($titre)
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    /**
+     * Get titre
+     *
+     * @return string
+     */
+    public function getTitre()
+    {
+        return $this->titre;
+    }
+
+    /**
+     * Set contenu
+     *
+     * @param string $contenu
+     *
+     * @return Page
+     */
+    public function setContenu($contenu)
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    /**
+     * Get contenu
+     *
+     * @return string
+     */
+    public function getContenu()
+    {
+        return $this->contenu;
+    }
+
+    /**
+     * Set lien
+     *
+     * @param string $lien
+     *
+     * @return Page
+     */
+    public function setLien($lien)
+    {
+        $this->lien = $lien;
+
+        return $this;
+    }
+
+    /**
+     * Get lien
+     *
+     * @return string
+     */
+    public function getLien()
+    {
+        return $this->lien;
+    }
+
+    /**
+     * Set topPublic
+     *
+     * @param boolean $topPublic
+     *
+     * @return Page
+     */
+    public function setTopPublic($topPublic)
+    {
+        $this->topPublic = $topPublic;
+
+        return $this;
+    }
+
+    /**
+     * Get topPublic
+     *
+     * @return bool
+     */
+    public function getTopPublic()
+    {
+        return $this->topPublic;
+    }
+
+    /**
+     * Set topPrivate
+     *
+     * @param boolean $topPrivate
+     *
+     * @return Page
+     */
+    public function setTopPrivate($topPrivate)
+    {
+        $this->topPrivate = $topPrivate;
+
+        return $this;
+    }
+
+    /**
+     * Get topPrivate
+     *
+     * @return bool
+     */
+    public function getTopPrivate()
+    {
+        return $this->topPrivate;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     *
+     * @return Page
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
+
---
+++ src/CMSBundle/Form/PageType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace CMSBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PageType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('titre')->add('contenu')->add('lien')->add('topPublic',null,['attr'=>['class']])->add('topPrivate')->add('password');
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CMSBundle\Entity\Page'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'cmsbundle_page';
+    }
+
+
+}
